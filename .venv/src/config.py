b6b86d71b7915a62802f9aaa624932aca779cac6
Yeounil <105293974+Yeounil@users.noreply.github.com>
2025-05-03 14:52:43 +0900

    Delete .venv/src/config.py

--- .venv/src/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-config = {
-    "processes": [
-        {"pid": 1, "arrival_time": 0, "burst_time": 5, "priority": 3},
-        {"pid": 2, "arrival_time": 1, "burst_time": 4, "priority": 2},
-        {"pid": 3, "arrival_time": 2, "burst_time": 6, "priority": 1},
-    ]
-}
