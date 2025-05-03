8f14902bf3e8eb25cfab11b6eef5aaf426f0c98a
Yeounil <105293974+Yeounil@users.noreply.github.com>
2025-05-03 15:58:05 +0900

    Update process.py

--- .venv/src/scheduler/process.py
+++ .venv/src/scheduler/process.py
@@ -1,10 +1,11 @@
 class Process:
-    def __init__(this, pid, arrival, burst):
-        this.pid = pid
-        this.arrival = arrival
-        this.burst = burst
-        this.remaining = burst
-        this.completion = 0
-        this.waiting = 0
-        this.turnaround = 0
-        this.normalized_tt = 0
+    def __init__(self, pid, arrival_time, burst_time):
+        self.pid = pid  # 프로세스 이름(번호)
+        self.arrival_time = arrival_time  # 도착시간
+        self.burst_time = burst_time  # 실행시간(일의 양)
+        self.remaining_time = burst_time  # 남은 실행시간
+        self.start_time = None  # 실제 시작시간
+        self.finish_time = None  # 종료시간
+        self.waiting_time = 0  # 대기시간
+        self.turn_around_time = 0  # 반환시간
+        self.normalized_TT = 0  # NTT
