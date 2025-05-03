4019d8c8a32d41b6dfc5af504b40457b13729d2b
white7112619 <white7112@naver.com>
2025-05-03 14:48:20 +0900

    Update rr.py

--- .venv/src/scheduler/algorithms/rr.py
+++ .venv/src/scheduler/algorithms/rr.py
@@ -10,7 +10,6 @@ class RoundRobin:
         ready_queue.sort(key=lambda p: p['arrival_time'])
 
         while ready_queue or queue:
-            # 도착한 프로세스 추가
             while ready_queue and ready_queue[0]['arrival_time'] <= time:
                 queue.append(ready_queue.pop(0))
 
@@ -21,16 +20,17 @@ class RoundRobin:
             for core in cores:
                 if not queue:
                     break
-                if not core.timeline or core.timeline[-1][0] <= time:
+                if core.available_at <= time:
                     process = queue.pop(0)
                     start_time = time
                     run_time = min(self.quantum, process['burst_time'])
-                    core.timeline.append((start_time, process['pid']))
-
+                    core.timeline.append((start_time, process['pid'], run_time))
                     process['burst_time'] -= run_time
-                    time = start_time + run_time
-
+                    core.available_at = start_time + run_time
                     if process['burst_time'] > 0:
+                        process['arrival_time'] = core.available_at
+                        queue.append(process)
+            time += 1
                         # 아직 남은 시간 있으면 다시 큐에 넣음
                         process['arrival_time'] = time
                         queue.append(process)
