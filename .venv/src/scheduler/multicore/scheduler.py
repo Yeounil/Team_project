abca0b618e8856f10f5245726be64a330292d6d0
Yeounil <105293974+Yeounil@users.noreply.github.com>
2025-05-03 15:50:48 +0900

    Update scheduler.py

--- .venv/src/scheduler/multicore/scheduler.py
+++ .venv/src/scheduler/multicore/scheduler.py
@@ -3,8 +3,21 @@ class Core:
         self.core_id = core_id
         self.current_process = None
         self.timeline = []  # (start_time, pid)
-        self.next_free_time = 0
+        self.next_free_time = 0 # 현재 작업이 끝나는 시간
+        self.core_type = core_type  # 'P' 또는 'E'
+        self.is_idle = True  # 현재 유휴상태 여부
+        self.used_time = 0  # 사용 시간
+        self.startup_count = 0  # 시동 횟수
+        self.last_used = -1  # 마지막 사용 시점 -> 마지막 사용시점으로부터 몇초가 지나면 다시 idle(절전)상태로 진입할지 기준 결정 필요한가?
 
+        if core_type == 'P':
+            self.performance = 2
+            self.power_rate = 3
+            self.startup_power = 0.5
+        else:
+            self.performance = 1
+            self.power_rate = 1
+            self.startup_power = 0.1
 
 class MultiCoreScheduler:
     def __init__(self, core_count, scheduler):
