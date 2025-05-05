9798eb3e10d3f05ee307f9b21e3a5948515902a8
kafuchino33 <91010579+kafuchino3388@users.noreply.github.com>
2025-05-06 02:51:48 +0900

    Update hrrn.py

--- .venv/src/scheduler/algorithms/hrrn.py
+++ .venv/src/scheduler/algorithms/hrrn.py
@@ -1,11 +1,11 @@
 from scheduler.process import Process
 from scheduler.multicore.scheduler import Core
 
-
 class HRRN:
     def schedule(self, ready_queue, pcores, ecores):
         cores = pcores + ecores
         time = 0
+
         # 각 프로세스에 executed(실행) 플래그 초기화
         for p in ready_queue:
             p.executed = False
@@ -13,6 +13,9 @@ class HRRN:
         # 아직 실행되지 않은 모든 프로세스가 실행될 때까지 반복함
         while not all(p.executed for p in ready_queue):
             # 현재 시간까지 도착은 했으나 아직 실행안된  프로세스를 골라냄
+            for core in cores:
+                if core.next_free_time <= time:
+                    core.is_idle = True
             ready = [p for p in ready_queue if p.arrival_time <= time and not p.executed]
             # 만약에 아직 도착한 프로세스가 없다면, time을 1올려 다시 확인
             if not ready:
@@ -25,6 +28,7 @@ class HRRN:
                 p.response_ratio = (wait + p.burst_time) / p.burst_time
             # "응답비율"이 높은 순으로 정렬함
             ready.sort(key=lambda p: p.response_ratio, reverse=True)
+
             # 할당 플래그
             assigned = False
 
@@ -40,26 +44,34 @@ class HRRN:
                     if core.core_type == 'P':
                         burst = (proc.burst_time + 1) // 2
                     else:
-                        busrt = proc.burst_time
+                        burst = proc.burst_time
 
-                    # 만약 코어가 놀고 있다가 실행 된다면 시동 전력을 계산함
-                    if core.next_free_time < time:
-                        core.total_power += core.startup_power
-                    core.total_power += core.power_rate * burst
-
-                    start = time
+                    start = max(time, proc.arrival_time)
                     finish = start + burst
+
+                    # 시동 전력 계산 부분
+                    # 만약 코어가 놀고 있다가 실행 된다면 시동 전력을 계산함
+                    if core.is_idle:
+                        core.total_power += core.startup_power
+                        core.startup_count += 1
+                    # 실행 전력 계산 부분
+                    core.total_power += core.power_rate * burst
+                    core.is_idle = False
+
                     core.timeline.append((start, proc.pid, burst))
                     # 코어가 일을 끝낸 시간을 알려줌. 즉 코어의 다음 빈 시간을 갱신하는 부분
                     core.next_free_time = finish
 
-                    proc.start_time = start
-                    proc.finish_time = finish
-                    proc.waiting_time = start - proc.arrival_time
-                    proc.turn_around_time = finish - proc.arrival_time
-                    proc.normalized_TT = proc.turn_around_time / proc.burst_time
+                    proc.start_time        = start
+                    proc.finish_time       = finish
+                    proc.waiting_time      = start - proc.arrival_time
+                    proc.turn_around_time  = finish - proc.arrival_time
+                    proc.normalized_TT     = proc.turn_around_time / proc.burst_time
                     # 할당 플래그
                     assigned = True
+                    break
             # 할당 플래그로 할당된 프로세스가 없다면 time을 1로 증가 시켜줌
             if not assigned:
+                time = min(core.next_free_time for c in cores)
+            else:
                 time += 1
