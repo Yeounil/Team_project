8283d4543c98c1522871570ad113ac3bb04687f4
kafuchino33 <91010579+kafuchino3388@users.noreply.github.com>
2025-05-07 16:49:36 +0900

    Update scheduler.py

--- .venv/src/scheduler/multicore/scheduler.py
+++ .venv/src/scheduler/multicore/scheduler.py
@@ -3,13 +3,11 @@ from scheduler import process
 class Core:
     def __init__(self, core_id, core_type):
         self.core_id = core_id
-        self.current_process = None
         self.timeline = []  # (start_time, pid, duration)
         self.next_free_time = 0 # 현재 작업이 끝나는 시간
         self.core_type = core_type  # 'P' 또는 'E'
         self.is_idle = True  # 현재 유휴상태 여부
         self.total_power = 0  # 총 파워
-        self.startup_count = 0  # 시동 횟수
 
         if core_type == 'P':
             self.performance = 2
